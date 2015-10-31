5e6c6773c4a21004204e27ec1e3939024f6912ff
Samuel Havard <SamuelHavard@yahoo.com>
2015-10-31 04:10:47 -0400

    add BaseAdapter / MovieAdapter

--- app/src/main/java/com/samuelhavard/popularmovies/MovieActivity.java
+++ app/src/main/java/com/samuelhavard/popularmovies/MovieActivity.java
@@ -1,24 +1,23 @@
 package com.samuelhavard.popularmovies;
 
 import android.app.Activity;
-import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.GridView;
-import android.widget.ListView;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import java.util.Arrays;
 
-import butterknife.Bind;
 
 /**
  * Created by samue_000 on 10/31/2015.
  */
 public class MovieActivity extends Activity {
     private MovieData[] mMovieData;
+
+    RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,10 +27,15 @@ public class MovieActivity extends Activity {
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.MOVIE_DATA);
         mMovieData = Arrays.copyOf(parcelables, parcelables.length, MovieData[].class);
 
-        GridView gridView = (GridView) findViewById(R.id.mainActivity_GridView);
+       // GridView gridView = (GridView) findViewById(R.id.mainActivity_GridView);
 
-       // MovieAdapter adapter = new MovieAdapter(this, mMovieData);
-        gridView.setAdapter(new MovieAdapter(this, mMovieData));
+        MovieAdapter adapter = new MovieAdapter(this, mMovieData);
+        //gridView.setAdapter(new MovieAdapter(this, mMovieData));
+        mRecyclerView.setAdapter(adapter);
+
+        //RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        mRecyclerView.setHasFixedSize(true);
     }
 
 //    @Override
--- app/src/main/java/com/samuelhavard/popularmovies/MovieAdapter.java
+++ app/src/main/java/com/samuelhavard/popularmovies/MovieAdapter.java
@@ -1,18 +1,18 @@
 package com.samuelhavard.popularmovies;
 
 import android.content.Context;
-import android.view.LayoutInflater;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
 
+
 /**
  * Created by samue_000 on 10/31/2015.
  */
-public class MovieAdapter extends BaseAdapter {
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder> {
 
     private Context mContext;
     private MovieData[] mMovieData;
@@ -23,48 +23,40 @@ public class MovieAdapter extends BaseAdapter {
     }
 
     @Override
-    public int getCount() {
-        return mMovieData.length;
+    public MovieAdapter.MovieViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        return null;
     }
 
     @Override
-    public Object getItem(int position) {
-        return mMovieData[position];
+    public void onBindViewHolder(MovieAdapter.MovieViewHolder movieViewHolder, int i) {
+
     }
 
     @Override
-    public long getItemId(int position) {
+    public int getItemCount() {
         return 0;
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ViewHolder holder;
-        ImageView imageView;
+    public class MovieViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+        public ImageView mPoster;
 
-        if (convertView == null) {
-            convertView = LayoutInflater.from(mContext).inflate(R.layout.poster_image_item, null);
-            holder = new ViewHolder();
-            holder.posterImageView = (ImageView) convertView.findViewById(R.id.backgroundImageView);
+        public MovieViewHolder(View itemView) {
+            super(itemView);
 
-            convertView.setTag(holder);
-        } else {
-            holder = (ViewHolder) convertView.getTag();
+            mPoster = (ImageView) itemView.findViewById(R.id.backgroundImageView);
+            itemView.setOnClickListener(this);
         }
 
-        imageView = (ImageView) convertView.findViewById(R.id.backgroundImageView);
-        MovieData movie = mMovieData[position];
-        Picasso.with(mContext)
-                .load(R.string.tmdb_url + movie.getImage())
-                .into(imageView);
-        holder.posterImageView.setId(R.id.backgroundImageView);
+        public void bindMovie(MovieData movie) {
+            Picasso.with(mContext).load(R.string.tmdb_url + movie.getImage()).into(mPoster);
+            //mPoster.setImageResource();
+        }
 
+        @Override
+        public void onClick(View v) {
 
-        return convertView;
-    }
-
-    private static class ViewHolder {
-        ImageView posterImageView;
+        }
     }
 }
 
